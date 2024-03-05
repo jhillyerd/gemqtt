4fa43ab58c170266559ffceadecfa287bdca7f58
James Hillyerd <james@hillyerd.com>
2024-03-04 16:37:44 -0800

    gleam 1.0 (#4)
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- gleam.nix
+++ gleam.nix
@@ -10,20 +10,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gleam";
-  version = "1.0.0-rc2";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "gleam-lang";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-WxdywmSn8U8Dth0T0guJrdU9w+sgyQLVsjyAM1Jjoa8=";
+    hash = "sha256-gPlRihwK+J7s1SeymfVdVo/KIV+eEqxlLVOgsDWW9yo=";
   };
 
   nativeBuildInputs = [ git pkg-config ];
 
   buildInputs = [ openssl ];
 
-  cargoHash = "sha256-NsIyP2vTzuyiz8UmparUI7FXLyt7CTNBBoa3q9OWujE=";
+  cargoHash = "sha256-ouu4Y1085dGSM7kGIWE+hBde6ZUOA1fO0AcHYXPOWzo=";
 
   passthru.updateScript = nix-update-script { };
 }
